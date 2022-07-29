069fa3c80e3a2941d4ea8265b043f1d5c3f4e091
shocker945516 <shocker945516@gmail.com>
2022-07-28 21:24:18 -0700

    602

--- src/main/java/com/company_mngm_sys/demo/web/ClientsController.java
+++ src/main/java/com/company_mngm_sys/demo/web/ClientsController.java
@@ -1,26 +1,35 @@
 package com.company_mngm_sys.demo.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.company_mngm_sys.demo.dao.ClientRepository;
 import com.company_mngm_sys.demo.entity.Clients;
 
-@RestController
+@Controller
 @RequestMapping("/clients")
 public class ClientsController {
 
     @Autowired
     ClientRepository clientRepo;
 
+    @GetMapping
+    public String displayClients(Model model) {
+        List<Clients> clients = clientRepo.findAll();
+        model.addAttribute("clientList", clients);
+        return "clients/clients_list";
+    }
+
     @GetMapping("/new")
     public String displayClientForm(Model model) {
         model.addAttribute("clients", new Clients());
-        return "/clients_new";
+        return "clients/clients_new";
     }
 
     @PostMapping("/save")
